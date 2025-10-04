52fa414894b504f1573ec861338319ae8ff12333
NicoUrrego <nicolas.urregog@uniagustiniana.edu.co>
2025-10-04 12:59:35 -0500

    Commit print

--- src/main/java/org/agusdevs/Main.java
+++ src/main/java/org/agusdevs/Main.java
@@ -1,8 +1,57 @@
 package org.agusdevs;
 
 public class Main {
+
+    public void printString(String txt){
+
+        try {
+
+            System.out.println(txt);
+        } catch (Exception e) {
+
+            System.out.println(e);
+        }
+    }
+
+    public void printInt(int n){
+
+        try {
+
+            System.out.println(n);
+        } catch (Exception e) {
+
+            System.out.println(e);
+        }
+    }
+
+    public void printDouble(Double d){
+
+        try {
+
+            System.out.println(d);
+        } catch (Exception e) {
+
+            System.out.println(e);
+        }
+    }
+
+    public void printBoolean(Boolean b){
+
+        try {
+
+            System.out.println(b);
+        } catch (Exception e) {
+
+            System.out.println(e);
+        }
+    }
     public static void main(String[] args) {
 
+        Main main = new Main();
 
+        main.printString("Hello World");
+        main.printInt(15);
+        main.printDouble(123.45);
+        main.printBoolean(true);
     }
 }
